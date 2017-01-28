33f498889656e869a8cea2989bd06a36a14270f9
Kristin Dragos <KristinDragos@gmail.com>
2017-01-27 22:12:21 -0800

    Added basic player.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public GameObject localCamera;
+	public GameObject vision;
 
 	private Vector3 offset;
 
 	void Start() {
 		localCamera = GameObject.FindGameObjectWithTag("localCamera");
 
-		Vector3 newLocation = new Vector3(gameObject.transform.position.x, localCamera.transform.position.y, gameObject.transform.position.z);
-		localCamera.transform.position = newLocation;
+		//Vector3 newLocation = new Vector3(gameObject.transform.position.x, localCamera.transform.position.y, gameObject.transform.position.z);
+		localCamera.transform.position = vision.transform.position;
 		offset = localCamera.transform.position - gameObject.transform.position;
 	}
 
