c74bbbd3ca9fa29fc456e88e98723301c5ff5c7d
Kristin Dragos <KristinDragos@gmail.com>
2017-01-27 20:57:51 -0800

    Added base project.
    FEATURE: Added GoogleVR sdk for Cardboard and Daydream VR capability.
    FEATURE: Added iTween for movement.

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public GameObject localCamera;
+
+	private Vector3 offset;
+
+	void Start() {
+		localCamera = GameObject.FindGameObjectWithTag("localCamera");
+
+		Vector3 newLocation = new Vector3(gameObject.transform.position.x, localCamera.transform.position.y, gameObject.transform.position.z);
+		localCamera.transform.position = newLocation;
+		offset = localCamera.transform.position - gameObject.transform.position;
+	}
+
+	void LateUpdate() {
+		localCamera.transform.position = gameObject.transform.position + offset;
+	}
+}
